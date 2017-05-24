bd52a9d75f1c20071ffb08226f289f5fc7f2917a
Dan <drbrown01@yahoo.com>
2017-05-23 21:32:06 -0500

    added models for users and secrets, performed migrations

--- dojo_secrets/dojo_secrets/apps/main/models.py
+++ dojo_secrets/dojo_secrets/apps/main/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from . import views
 
 # Create your models here.
+class Users(models.Model):
+    Fname = models.CharField(max_length=255)
+    Lname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    
+
+class Secrets(models.Model):
+    secrets = models.CharField(max_length=255)
+    likes = models.IntegerField()
