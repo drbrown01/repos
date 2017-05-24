6eb163c201f25f8e84ac8732ab221084834da985
Dan <drbrown01@yahoo.com>
2017-05-23 21:46:46 -0500

    added models and migrated timestamps

--- dojo_secrets/dojo_secrets/apps/main/models.py
+++ dojo_secrets/dojo_secrets/apps/main/models.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from . import views
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 class Users(models.Model):
@@ -9,8 +12,11 @@ class Users(models.Model):
     Lname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Secrets(models.Model):
     secrets = models.CharField(max_length=255)
     likes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
