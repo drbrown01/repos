4670db33e25be0bc568b27761ca936d9d39742cf
Dan <drbrown01@yahoo.com>
2017-05-25 19:42:14 -0500

    added create vs process

--- user_validations/apps/main/urls.py
+++ user_validations/apps/main/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
-    url(r'^process$', views.process),
+    url(r'^create$', views.create),
 ]
--- user_validations/apps/main/views.py
+++ user_validations/apps/main/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def create(request):
+
+    print request.POST
+    return redirect('/')
